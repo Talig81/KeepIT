b57382bd4c20a23f0726d462c3eeac56ffa2b2ed
Sara Pereira <saralexx@gmail.com>
2019-10-29 10:49:42 +0000

    mudança de nomes de ficheiros

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:keep_it/login_screen.dart';
+import 'package:keep_it/home_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: LoginScreen(),
+      home: HomeScreen(),
     );
   }
 }
