00023ca272bade45dedcafdff98ddff24b3ea437
Sara Pereira <saralexx@gmail.com>
2019-12-26 23:37:19 +0000

    scrollable receipts

--- lib/Dashboard/receipts2.dart
+++ lib/Dashboard/receipts2.dart
@@ -15,8 +15,8 @@ class _Receipts extends State<Receipts> {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
-        Expanded(
-          flex: 0,
+        Container(
+          height: MediaQuery.of(context).size.height/2,
           child: receipts(context),
         ),
         Column(
@@ -26,20 +26,22 @@ class _Receipts extends State<Receipts> {
               child: Divider(color: Colors.teal[800]),
             ),
             Container(
-              padding: EdgeInsets.only(left: 155),
+                padding: EdgeInsets.only(left: 155),
                 child: Text(
-              "Total = 1234",
-              style: TextStyle(
-                  color: Colors.teal[800], fontWeight: FontWeight.bold, fontSize: 30),
-              textAlign: TextAlign.right,
-            ))
+                  "Total = 1234",
+                  style: TextStyle(
+                      color: Colors.teal[800],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                  textAlign: TextAlign.right,
+                ))
           ],
         )
       ],
     );
   }
 
-  Widget receipt() => new ListTile(
+  Widget receipt() => new ListTile( //aqui vai ter que se meter a cena das faturas 
         contentPadding: EdgeInsets.symmetric(
           horizontal: ScreenUtil.instance.setWidth(10.0),
           vertical: ScreenUtil.instance.setHeight(4.0),
@@ -82,41 +84,46 @@ class _Receipts extends State<Receipts> {
         ),
       );
 
-  Widget receipts(BuildContext context) => new CustomScrollView(
-        //scrollDirection: Axis.horizontal,
-        //scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        slivers: <Widget>[
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (BuildContext context, int index) {
-                return Container(
-                  alignment: Alignment.center,
-                  padding: EdgeInsets.all(
-                    ScreenUtil.instance.setWidth(20),
-                  ),
-                  //color: Colors.teal[100 * (index % 9)],
-                  child: IndexedSemantics(index: 0, child: receipt()),
-                );
-              },
-              childCount: 5,
-            ),
+  Widget receipts(BuildContext context) => ListView.separated(
+          physics: ClampingScrollPhysics(),
+          itemCount: 10,
+          separatorBuilder: (BuildContext context, int index) => Divider(
+            indent: ScreenUtil.instance.setWidth(90),
+            endIndent: ScreenUtil.instance.setWidth(80),
+            color: Colors.teal[800],
           ),
-        ],
-
-        // separatorBuilder: (BuildContext context, int index) => Divider(
-        //   indent: ScreenUtil.instance.setWidth(80),
-        //   endIndent: ScreenUtil.instance.setWidth(80),
-        //   color: Colors.teal[800],
-        // ),
-        // itemBuilder: (BuildContext context, int index) {
-        //   return Center(
-        //     //color: Colors.green[100],
-        //     child: Container(
-        //       width: MediaQuery.of(context).size.width - 100,
-        //       height: 80,
-        //       child: receipt(),
-        //     ),
-        //   );
-      );
+          itemBuilder: (BuildContext context, int index) {
+            return Center(
+              //color: Colors.green[100],
+              child: Container(
+                width: MediaQuery.of(context).size.width - 100,
+                height: 80,
+                child: receipt(),
+              ),
+            );
+          },
+  );
 }
+
+//scrollDirection: Axis.horizontal,
+//new CustomScrollView(
+//scrollDirection: Axis.horizontal,
+//   shrinkWrap: true,
+//   slivers: <Widget>[
+//     SliverList(
+//       delegate: SliverChildBuilderDelegate(
+//         (BuildContext context, int index) {
+//           return Container(
+//             alignment: Alignment.center,
+//             padding: EdgeInsets.all(
+//               ScreenUtil.instance.setWidth(20),
+//             ),
+//             //color: Colors.teal[100 * (index % 9)],
+//             child: IndexedSemantics(index: 0, child: receipt()),
+//           );
+//         },
+//         childCount: 5,
+//       ),
+//     ),
+//   ],
+// );
